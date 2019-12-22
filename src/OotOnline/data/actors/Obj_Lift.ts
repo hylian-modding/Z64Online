c812ac672dcfd65f84e40fab5202916ff2b9295d
MelonSpeedruns <melonspeedruns@outlook.com>
2019-12-22 18:36:00 -0500

    Create Obj_Lift.ts

---
+++ src/OotOnline/data/actors/Obj_Lift.ts
@@ -0,0 +1,11 @@
+import { ActorHookBase, HookInfo } from '../ActorHookBase';
+
+class Obj_Lift extends ActorHookBase {
+  constructor() {
+    super();
+    this.actorID = 0x012c;
+    this.hooks.push(new HookInfo(0x150, 0x1));
+  }
+}
+
+module.exports = new Obj_Lift();
