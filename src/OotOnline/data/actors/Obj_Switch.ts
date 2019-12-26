b546621b7d8dc5430b6c45f6c531990a36e80876
MelonSpeedruns <melonspeedruns@outlook.com>
2019-12-26 18:48:34 -0500

    Update Obj_Switch.ts

--- src/OotOnline/data/actors/Obj_Switch.ts
+++ src/OotOnline/data/actors/Obj_Switch.ts
@@ -4,6 +4,7 @@ class Obj_Switch extends ActorHookBase {
   constructor() {
     super();
     this.actorID = 0x012A;
+    this.hooks.push(new HookInfo(0x150, 0x1));
     this.hooks.push(new HookInfo(0x154, 0x4, true));
   }
 }
