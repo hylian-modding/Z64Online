facdf5426312ace03518e8fac209e228e672bc71
Denoflions <denvstheworld@gmail.com>
2020-09-18 05:10:02 -0400

    Redundant.

--- src/OotOnline/data/OotoSaveData.ts
+++ src/OotOnline/data/OotoSaveData.ts
@@ -826,7 +826,6 @@ export function mergeEquipmentData(
     if (side === ProxySide.CLIENT) {
       if (!SEEN_DEKU_SHIELD) {
         save.dekuShield = true;
-        SEEN_DEKU_SHIELD = true;
         ModLoader.emulator.rdramWriteBit8(LOST_ITEM_FLAGS, LOSEABLE_GEAR.SEEN_DEKU_SHIELD, true);
       }else{
         save.dekuShield = ModLoader.emulator.rdramReadBit8(LOST_ITEM_FLAGS, LOSEABLE_GEAR.CURRENTLY_HAS_DEKU_SHIELD);
@@ -842,7 +841,6 @@ export function mergeEquipmentData(
     if (side === ProxySide.CLIENT) {
       if (!SEEN_HYLIAN_SHIELD) {
         save.hylianShield = true;
-        SEEN_HYLIAN_SHIELD = true;
         ModLoader.emulator.rdramWriteBit8(LOST_ITEM_FLAGS, LOSEABLE_GEAR.SEEN_HYLIAN_SHIELD, true);
       }else{
         save.hylianShield = ModLoader.emulator.rdramReadBit8(LOST_ITEM_FLAGS, LOSEABLE_GEAR.CURRENTLY_HAS_HYLIAN_SHIELD);
