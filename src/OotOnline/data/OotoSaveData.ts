b2be77f7ea7987a79aaffcf253e08091daa5dc32
Denoflions <denvstheworld@gmail.com>
2020-09-18 23:37:26 -0400

    Revert "Try to fix the shield thing properly."
    This reverts commit 2b2a1898f255d607ecd903f501f5618d68d020af.

--- src/OotOnline/data/OotoSaveData.ts
+++ src/OotOnline/data/OotoSaveData.ts
@@ -322,15 +322,6 @@ export let SEEN_MASK_OF_TRUTH: boolean = false;
 export let FIRST_HEART_CONTAINER_SET: boolean = false;
 export let SEEN_DEKU_SHIELD: boolean = false;
 export let SEEN_HYLIAN_SHIELD: boolean = false;
-export let LOST_ITEM_FLAGS: number = 0x8011B878;
-
-export const enum LOSEABLE_GEAR{
-  SEEN_DEKU_SHIELD,
-  SEEN_HYLIAN_SHIELD,
-  SEEN_MASK_OF_TRUTH,
-  CURRENTLY_HAS_DEKU_SHIELD,
-  CURRENTLY_HAS_HYLIAN_SHIELD
-}
 
 // As much as I want to pull some Object.keys bullshit here to make writing this less verbose, I don't want any sneaky bugs.
 // So, we write it all verbose as hell.
@@ -500,7 +491,6 @@ export function mergeInventoryData(
       save.childTradeItem = incoming.childTradeItem;
       if (save.childTradeItem === InventoryItem.MASK_OF_TRUTH) {
         SEEN_MASK_OF_TRUTH = true;
-        ModLoader.emulator.rdramWriteBit8(LOST_ITEM_FLAGS, 2, true);
       }
     }
   }
@@ -790,9 +780,6 @@ export function mergeEquipmentData(
   side: ProxySide,
   lobby: string
 ) {
-  SEEN_DEKU_SHIELD = ModLoader.emulator.rdramReadBit8(LOST_ITEM_FLAGS, LOSEABLE_GEAR.SEEN_DEKU_SHIELD);
-  SEEN_HYLIAN_SHIELD = ModLoader.emulator.rdramReadBit8(LOST_ITEM_FLAGS, LOSEABLE_GEAR.SEEN_HYLIAN_SHIELD);
-  SEEN_MASK_OF_TRUTH = ModLoader.emulator.rdramReadBit8(LOST_ITEM_FLAGS, LOSEABLE_GEAR.SEEN_MASK_OF_TRUTH);
   // Swords
   if (incoming.kokiriSword) {
     if (save.kokiriSword !== true && side === ProxySide.SERVER) {
@@ -827,9 +814,6 @@ export function mergeEquipmentData(
       if (!SEEN_DEKU_SHIELD) {
         save.dekuShield = true;
         SEEN_DEKU_SHIELD = true;
-        ModLoader.emulator.rdramWriteBit8(LOST_ITEM_FLAGS, LOSEABLE_GEAR.SEEN_DEKU_SHIELD, true);
-      }else{
-        save.dekuShield = ModLoader.emulator.rdramReadBit8(LOST_ITEM_FLAGS, LOSEABLE_GEAR.CURRENTLY_HAS_DEKU_SHIELD);
       }
     } else if (side === ProxySide.SERVER) {
       save.dekuShield = true;
@@ -843,9 +827,6 @@ export function mergeEquipmentData(
       if (!SEEN_HYLIAN_SHIELD) {
         save.hylianShield = true;
         SEEN_HYLIAN_SHIELD = true;
-        ModLoader.emulator.rdramWriteBit8(LOST_ITEM_FLAGS, LOSEABLE_GEAR.SEEN_HYLIAN_SHIELD, true);
-      }else{
-        save.hylianShield = ModLoader.emulator.rdramReadBit8(LOST_ITEM_FLAGS, LOSEABLE_GEAR.CURRENTLY_HAS_HYLIAN_SHIELD);
       }
     } else if (side === ProxySide.SERVER) {
       save.hylianShield = true;
