3027401a03ddc38c6b9e66c4625461bd95e5ab56
CrookedPoe <djinsomniapro@gmail.com>
2020-10-01 20:38:18 -0500

    Added puppet.h

---
+++ src/OotOnline/c/Puppet-CrookedPoe/defines_limbs.h
@@ -0,0 +1,43 @@
+#define WAIST 1
+#define RTHIGH 3
+#define RSHIN 4
+#define RFOOT 5
+#define LTHIGH 6
+#define LSHIN 7
+#define LFOOT 8
+#define HEAD 10
+#define HAT 11
+#define COLLAR 12
+#define LSHOULDER 13
+#define LFOREARM 14
+#define RSHOULDER 16
+#define RFOREARM 17
+#define TORSO 20
+#define RHAND 18
+#define LHAND 15
+#define SHEATH 19
+
+/* Left and Right are from Link's POV */
+#define LIMB_ROOT 0 /* No Display List */
+#define LIMB_WAIST 1 /* Waist */
+#define LIMB_LOW_AFFECTOR 2 /* No Display List */
+#define LIMB_THIGH_R 3 /* Right Thigh */
+#define LIMB_SHIN_R 4 /* Right Shin */
+#define LIMB_FOOT_R 5 /* Right Foot */
+#define LIMB_THIGH_L 6 /* Left Thigh */
+#define LIMB_SHIN_L 7 /* Left Shin */
+#define LIMB_FOOT_L 8 /* Left Foot */
+#define LIMB_HIGH_AFFECTOR 9 /* No Display List */
+#define LIMB_HEAD 10 /* Head */
+#define LIMB_HAT 11 /* Hat Tail */
+#define LIMB_COLLAR 12 /* Tunic Collar */
+#define LIMB_SHOULDER_L 13 /* Left Shoulder */
+#define LIMB_FOREARM_L 14 /* Left Forearm */
+#define LIMB_HAND_L 15 /* Left Hand */
+#define LIMB_SHOULDER_R 16 /* Right Shoulder */
+#define LIMB_FOREARM_R 17 /* Right Forearm */
+#define LIMB_HAND_R 18 /* Right Hand */
+#define LIMB_SHEATH 19 /* Sheath / Sheathed Sword -- Back Equip Animation Bone */
+#define LIMB_TORSO 20 /* Torso */
+#define LIMB_TOTAL 21 /* Total Limb Count -- including 0 */
+#define LIMB_TOTAL_DLIST 18 /* Total Limb Count -- only the ones that use display lists to generate matrix table */
