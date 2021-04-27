a16b47df4727464aa06cc5764b4c0158d442bec3
Denoflions <denvstheworld@gmail.com>
2021-04-27 16:44:56 -0400

    Missed some

--- src/OotOnline/OotOnlineStorageBase.ts
+++ src/OotOnline/OotOnlineStorageBase.ts
@@ -1,8 +1,5 @@
-import { SavedLogEntry } from './data/keys/KeyLogEntry';
-
 export class OotOnlineStorageBase {
   constructor() {}
 
   playerModelCache: any = {};
-  changelog: Array<SavedLogEntry> = new Array<SavedLogEntry>();
 }
