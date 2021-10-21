f2f9b5989e037312177e0691ef0f06eca6860719
Denoflions <denvstheworld@gmail.com>
2021-10-21 12:49:11 -0400

    ifdef out more code in prod

--- src/Z64Online/Z64Online.ts
+++ src/Z64Online/Z64Online.ts
@@ -6,7 +6,7 @@ import { Z64_GAME } from "Z64Lib/src/Common/types/GameAliases";
 import { Z64LibSupportedGames } from "Z64Lib/API/Utilities/Z64LibSupportedGames";
 import { setCommonConfigInst, Z64O_Common_Config } from "./common/lib/Settings";
 
-export default class Z64Online implements IZ64GameMain{
+export default class Z64Online implements IZ64GameMain {
     @SidedProxy(ProxySide.CLIENT, path.resolve(__dirname, "common", "assets", "bootstrap.js"))
     assets!: any;
 
@@ -14,12 +14,15 @@ export default class Z64Online implements IZ64GameMain{
     OOT!: IZ64GameMain;
     @SidedProxy(ProxySide.UNIVERSAL, path.resolve(__dirname, "mm", "MMOnline.js"), "Z64Lib", () => { return Z64_GAME === Z64LibSupportedGames.MAJORAS_MASK })
     MM!: IZ64GameMain;
+    
+    // #ifdef IS_DEV_BUILD
     @SidedProxy(ProxySide.CLIENT, path.resolve(__dirname, "common", "Tests.js"), "Z64Lib")
     tests!: any;
+    // #endif
 
     ModLoader!: IModLoaderAPI;
 
-    init(){
+    init() {
         setCommonConfigInst(this.ModLoader.config.registerConfigCategory("Z64O_Common_Config") as Z64O_Common_Config);
         this.ModLoader.config.setData("Z64O_Common_Config", "nameplates", true);
         this.ModLoader.config.setData("Z64O_Common_Config", "muteNetworkedSounds", false);
--- src/Z64Online/common/Tests.ts
+++ src/Z64Online/common/Tests.ts
@@ -5,8 +5,10 @@ export default class Z64OTests{
 
     @Init()
     init(){
+        // #ifdef IS_DEV_BUILD
         CosmeticTests.onGoronTest();
         //CosmeticTests.onEquipmentTest();
+        // #endif
     }
 
 }
