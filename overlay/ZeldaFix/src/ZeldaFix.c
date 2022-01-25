56a8814a6917a8d63a2447e859ff0ba76766d5cb
Denoflions <denvstheworld@gmail.com>
2022-01-24 20:22:08 -0500

    Spitballing a Ganon fix

--- overlay/ZeldaFix/src/ZeldaFix.c
+++ overlay/ZeldaFix/src/ZeldaFix.c
@@ -10,6 +10,8 @@ void draw(void* thisx, GlobalContext* globalCtx)
     ((ActorFunc) haxPointer->draw)(thisx, globalCtx);
 }
 
+#define GANON_BATTLE_ARENA 0x004F
+
 void doInject(void* this, GlobalContext* globalCtx, uint32_t pointer){
     En_HaxBase* thisx = ((En_HaxBase*)this);
     haxPointer = thisx;
@@ -19,6 +21,7 @@ void doInject(void* this, GlobalContext* globalCtx, uint32_t pointer){
     thisx->draw = actor->draw;
     actor->update = &update;
     actor->draw = &draw;
+    if (globalCtx->sceneNum == GANON_BATTLE_ARENA) return;
     if (actor->category != ACTORLIST_CATEGORY_CHEST){
         Actor_RemoveFromCategory(globalCtx, &globalCtx->actorCtx, actor);
         Actor_AddToCategory(&globalCtx->actorCtx, actor, ACTORLIST_CATEGORY_CHEST);
